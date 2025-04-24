739f76ba3a120b9427b94e3d809810f479beb5fd
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2025-04-24 17:56:12 -0600

    v.1.0.1

--- src/layout/user-profile/join-team/join-team.tsx
+++ src/layout/user-profile/join-team/join-team.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './join-team.css';
-import logo from '../../assets/SISREP_LOGO_BLACK.svg';
+import logo from '../../../assets/SISREP_LOGO_BLACK.svg';
 
 const JoinTeam = () => {
   const handleButtonClick = () => {
@@ -10,30 +11,32 @@ const JoinTeam = () => {
 
   return (
     <div className="join-team-container">
-        <div className="header">
-          <h1>Únete a nuestro equipo</h1>
-          <img src={logo} alt="Logo" className="logo"></img>
+        <div className="header-join">
+          <h1 className="h1-join">Únete a nuestro equipo</h1>
+          <img src={logo} alt="Logo" className="logo-join"></img>
         </div>
 
-        <button 
-          className="button"
-          onClick={handleButtonClick}>
-          Enviar solicitud
-        </button>
-
-      <div className="container-card">
-        <div className="card card-blue columna">
+      <div className="container-card-join">
+        <div className="card-join card-blue columna">
           <h2>Participa activamente en la seguridad </h2>
           <p>¿Te gustaría asumir un rol más activo dentro de la plataforma?</p>
         </div>
-        <div className="card card-orange columna">
+        <div className="card-join card-orange columna">
           <h2>Administradores de la plataforma</h2>
           <p>Conviértete en administrador y ayuda a gestionar los reportes de incidentes.</p>
         </div>
-        <div className="card card-yellow columna">
+        <div className="card-join card-yellow columna">
           <h2>Monitorea y mejora</h2>
           <p>Tendrás la posibilidad de actualizar el estado de cada incidente, colaborar en su resolución y asegurarte de que la información fluya correctamente.</p>
         </div>
+      </div>
+
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button 
+          className="btn btn-primary"
+          onClick={handleButtonClick}>
+          Enviar solicitud
+        </button>
       </div>
     </div>
   );
