9506a8fac3ea51a53dcd542b127d1a590c2fa017
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2025-04-23 22:53:31 -0600

    v.1.0.0 del estilo

--- src/layout/user-profile/join-team/join-team.tsx
+++ src/layout/user-profile/join-team/join-team.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './join-team.css';
+import logo from '../../assets/SISREP_LOGO_BLACK.svg';
 
 const JoinTeam = () => {
   const handleButtonClick = () => {
@@ -9,15 +10,31 @@ const JoinTeam = () => {
 
   return (
     <div className="join-team-container">
-      <h2 className="join-team-title">Únete a nuestro equipo</h2>
-      <p className="join-team-subtitle">¿Te gustaría asumir un rol más activo dentro de la plataforma?</p>
-      <p className="join-team-subsubtitle">Conviértete en administrador y ayuda a gestionar los reportes de incidentes.</p>
-      <p className="join-team-body">Tendrás la posibilidad de actualizar el estado de cada incidente, colaborar en su resolución y asegurarte de que la información fluya correctamente.</p>
-      <button 
-        className="join-team-button"
-        onClick={handleButtonClick}>
-        Enviar solicitud
-      </button>
+        <div className="header">
+          <h1>Únete a nuestro equipo</h1>
+          <img src={logo} alt="Logo" className="logo"></img>
+        </div>
+
+        <button 
+          className="button"
+          onClick={handleButtonClick}>
+          Enviar solicitud
+        </button>
+
+      <div className="container-card">
+        <div className="card card-blue columna">
+          <h2>Participa activamente en la seguridad </h2>
+          <p>¿Te gustaría asumir un rol más activo dentro de la plataforma?</p>
+        </div>
+        <div className="card card-orange columna">
+          <h2>Administradores de la plataforma</h2>
+          <p>Conviértete en administrador y ayuda a gestionar los reportes de incidentes.</p>
+        </div>
+        <div className="card card-yellow columna">
+          <h2>Monitorea y mejora</h2>
+          <p>Tendrás la posibilidad de actualizar el estado de cada incidente, colaborar en su resolución y asegurarte de que la información fluya correctamente.</p>
+        </div>
+      </div>
     </div>
   );
 };
