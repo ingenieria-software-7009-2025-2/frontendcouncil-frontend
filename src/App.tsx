a1671e52aa141ba3c623d01da0d9606fe5fcf816
Alejandro_Jacome <Jacomedelgadoa@ciencias.unam.mx>
2025-04-29 00:13:24 -0600

    arreglo de app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react';
-import './style.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './layout/homepage/homepage';
-import NavbarComponent from './components/navbar/navbar';
-import UserProfilePage from './layout/user-profile/user-profile';
-import EditProfile from './layout/user-profile/edit-profile/edit-profile';
-import JoinTeam from './layout/user-profile/join-team/join-team';
-import ManageIncidents from './layout/admin-panel/manage-incidents.tsx/manage-incidents';
-import ManageUsers from './layout/admin-panel/manage-users/manage-users';
-import NotFoundPage from './layout/page-not-found/page-not-found';
-import ProtectedRoute from './routes/protected-route';
+import {useEffect, useState} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import NavbarComponent from "./components/navbar/navbar.tsx";
+import HomePage from "./layout/homepage/homepage.tsx";
+import UserProfilePage from "./layout/user-profile/user-profile.tsx";
+import EditProfile from "./layout/user-profile/edit-profile/edit-profile.tsx";
+import JoinTeam from "./layout/user-profile/join-team/join-team.tsx";
+import NotFoundPage from "./layout/page-not-found/page-not-found.tsx";
+import ManageIncidents from "./layout/admin-panel/manage-incidents.tsx/manage-incidents.tsx";
+import ManageUsers from "./layout/admin-panel/manage-users/manage-users.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -35,36 +33,12 @@ function App() {
           <div className="flex-1 bg-gray-200 p-4 ">
             <Routes>
               <Route path="/" element={<HomePage />} />
-
-              <Route path="/profile" element={
-                <ProtectedRoute isAuth={!!localStorage.getItem('authToken')}>
-                  <UserProfilePage />
-                </ProtectedRoute>
-              } />
-
-              <Route path="/edit-profile" element={
-                <ProtectedRoute isAuth={!!localStorage.getItem('authToken')}>
-                  <EditProfile />
-                </ProtectedRoute>
-              } />
-
-              <Route path="/join-team" element={
-                <ProtectedRoute isAuth={!!localStorage.getItem('authToken')}>
-                  <JoinTeam />
-                </ProtectedRoute>
-              } />
-
-              <Route path="/manage-incidents" element={
-                <ProtectedRoute isAuth={!!localStorage.getItem('authToken')}>
-                  <ManageIncidents />
-                </ProtectedRoute>
-              } />
-
-              <Route path="/manage-users" element={
-                <ProtectedRoute isAuth={!!localStorage.getItem('authToken')}>
-                  <ManageUsers />
-                </ProtectedRoute>
-              } />
+              <Route path="/profile" element={<UserProfilePage />} />
+              <Route path="/edit-profile" element={<EditProfile />} />
+              <Route path="/join-team" element={<JoinTeam />} />
+              <Route path="/*" element={<NotFoundPage />} />
+              <Route path="/manage-incidents" element={<ManageIncidents />} />
+              <Route path="/manage-users" element={<ManageUsers />} />
             </Routes>
           </div>
         </div>
